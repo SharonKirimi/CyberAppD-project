60cfeb1f33934afde4fc86dcf99b88f304303c38
Sharon Kirimi <kirimisharon@gmail.com>
2021-12-08 05:48:32 +0100

    model edits

--- models/post.js
+++ models/post.js
@@ -7,7 +7,7 @@ const postSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-		Owner: {
+		owner: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
 			required: true,
